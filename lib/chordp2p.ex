b04fa83406e21e95c34304ca33558f84c2c6e82a
jay137 <42896779+jay137@users.noreply.github.com>
2018-10-13 19:28:05 -0400

    Project Init

---
+++ lib/chordp2p.ex
@@ -0,0 +1,18 @@
+defmodule CP2P do
+  @moduledoc """
+  Documentation for CP2P.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CP2P.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/chordp2p_test.exs
@@ -0,0 +1,8 @@
+defmodule CP2PTest do
+  use ExUnit.Case
+  doctest CP2P
+
+  test "greets the world" do
+    assert CP2P.hello() == :world
+  end
+end
