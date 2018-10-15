7f9f1b202d24749c451530ceefd7c09faa0dafe9
jay137 <42896779+jay137@users.noreply.github.com>
2018-10-15 18:01:58 -0400

    Arguments parsed by CLI

---
+++ lib/chord_CLI.ex
@@ -0,0 +1,14 @@
+defmodule Chord_CLI do
+  require Logger
+
+  def main(args) do
+    # {_, noflag_opts, _} = OptionParser.parse(args)
+    Logger.debug(inspect(args) <> inspect(is_list(args)))
+
+    {num_nodes, _} = Integer.parse(Enum.at(args, 0))
+    {num_req, _} = Integer.parse(Enum.at(args, 1))
+    Logger.debug(inspect(num_nodes) <> inspect(num_req))
+
+    # TODO: Call start here (num_nodes, num_req)
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule CP2P.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: Chord_CLI],
       deps: deps()
     ]
   end
