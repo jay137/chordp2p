2435949b22fd8aec5a1fd90d8ddc8cae7d318e61
Mukul Chand Yadav <mchand.yadav@ufl.edu>
2018-10-16 19:51:24 -0400

    Fix child node not starting issue

--- lib/CP2P/node.ex
+++ lib/CP2P/node.ex
@@ -1,23 +1,24 @@
 defmodule CP2P.Node do
+
+  require Logger
+
   use GenServer
 
   def start_link(opts) do
-    # Logger.debug("Inside start_link " <> inspect(_MODULE_) <> " " <> "with options: " <> inspect(opts))
+    Logger.debug("#{inspect(__MODULE__)} Inside start_link  with options: #{inspect(opts)}")
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
   @impl true
-  def init(args) do
-    {:ok, %{}}
+  def init(num_req) do
+    Logger.debug("#{inspect(__MODULE__)} Inside init  num requests: #{inspect(num_req)}")
+    {:ok, %{req_left: num_req}}
   end
 
   @impl true
   def handle_call(:pop, _from, [head | tail]) do
+
     {:reply, head, tail}
   end
 
-  @impl true
-  def handle_cast({:push, item}, state) do
-    {:noreply, [item | state]}
-  end
 end
--- lib/chord_CLI.ex
+++ lib/chord_CLI.ex
@@ -2,14 +2,12 @@ defmodule Chord_CLI do
   require Logger
 
   def main(args) do
-    # {_, noflag_opts, _} = OptionParser.parse(args)
-    Logger.debug(inspect(args) <> inspect(is_list(args)))
 
     {num_nodes, _} = Integer.parse(Enum.at(args, 0))
     {num_req, _} = Integer.parse(Enum.at(args, 1))
-    Logger.debug(inspect(num_nodes) <> inspect(num_req))
+    Logger.debug("#{inspect(__MODULE__)} #{inspect(num_nodes)}  #{inspect(num_req)}")
 
     # TODO: Call start here (num_nodes, num_req)
-    ChordP2P.start(:normal, [num_nodes, num_req])
+    ChordP2P.start(:normal, %{numNodes: num_nodes, numReq: num_req})
   end
 end
